9190b6e90f0bdc4da2751b820e2bdc5c88b25815
william-vw <wvw@Williams-MBP.lan>
2024-06-02 13:28:42 -0400

    done 4 today

--- python/n3/fun/gen.py
+++ python/n3/fun/gen.py
@@ -44,43 +44,55 @@ class GenPython:
 
     def __gen_clause(self, rule_no, head, body, clause_no):
         clause = body.model.triple_at(clause_no)
-
-        in_params = self.__cur_params  # incoming parameters representing variables
+        
+        # incoming parameters representing variables
+        in_params = self.__cur_params
         # all incoming parameters
         own_params = in_params + ['model', 'state', 'ctu']
+        
+        # function representing this clause
         clause_fn = self.__builder.fn(rule_no, clause_no, own_params)
 
         # running example: clause = ?p :address ?a ; cur_vars = [ p, r ] ; head = ?p a :Person
 
-        # e.g., [ p, a ]
+        # ex: [ p, a ]
         own_vars = [v for _, v in self.__vars_triple(clause)]
 
         if clause_no < body.model.len() - 1:
-            # e.g., p, r, a
-            # (unique but keep ordering)
-            ctu_params = list(dict.fromkeys(self.__cur_params + own_vars))
+            # parameters for the ctu function; unique(prior + own vars)
+            # ex: p, r, a
+            ctu_params = list(dict.fromkeys(self.__cur_params + own_vars)) # keep order
+            # rest of ctu args (unrelated to vars)
             rest_args = [self.__builder.ref(v)
                          for v in ['model', 'state', 'ctu']]
             self.__cur_params = ctu_params
             # call next clause fn
             ctu_fn = self.__builder.fn_name(rule_no, clause_no+1)
         else:
-            # e.g., p
+            # only pass var needed by original ctu fn (head vars!)
+            # ex: p
             ctu_params = self.__vars_graph(head)
+            # rest of ctu args (unrelated to vars)
             rest_args = [self.__builder.ref('state')]
             # at the last clause, so call the original ctu
             ctu_fn = 'ctu'
 
-        # e.g., { 'p' : 's', 'a': 'o' }
+        self.__find_triple_call(clause_fn, clause, in_params, ctu_fn, ctu_params, rest_args)
+        self.__match_rule_calls(clause_fn, clause, in_params, ctu_fn, ctu_params, rest_args)
+
+        ast.fix_missing_locations(clause_fn)
+
+        return clause_fn
+    
+    def __find_triple_call(self, clause_fn, clause, in_params, ctu_fn, ctu_params, rest_args):
+        # ex: { 'p' : 's', 'a': 'o' }
         clause_vars = {v: i for i, v in self.__vars_triple(clause)}
 
         # arguments to be passed to ctu
-        # if next is clause: e.g., att_ref(t.s), ref(r), att_ref(t.o)
-        # if next is head: e.g., att_ref(t.s)
+        # if next is clause: ex: att_ref(t.s), ref(r), att_ref(t.o)
+        # if next is head: ex: att_ref(t.s)
         pass_args = [self.__builder.attr_ref('t', clause_vars[p]) if p in clause_vars else self.__builder.ref(p)
                      for p in ctu_params]
-
-        # - find matching triples
 
         # model.find will call a lambda
         # this lambda will itself call a ctu
@@ -112,15 +124,24 @@ class GenPython:
         search_call = self.__builder.fn_call(
             self.__builder.attr_ref('model', 'find'), call_args)
 
-        # add search call to the function's body
         self.__builder.fn_body_stmt(
             clause_fn, self.__builder.to_stmt(search_call))
-
-        # - call matching rule fns
+    
+    def __match_rule_calls(self, clause_fn, clause, in_params, ctu_fn, ctu_params, rest_args):
         matches = self.__matching_rules(clause)
         print("match", clause.p, matches)
 
         # TODO blank nodes vs. universals
+
+        # examples:
+        # 1/ head:  ?p a Person
+        #    match: ?p a Canadian
+        # 2/ head:  ?p a ?t (cur_vars=[t])
+        #    match: ?p a Canadian
+        # 3/ head:  ?p a Person (cur_vars=[p])
+        #    match: ?pe a ?t
+        # 4/ head:  ?p a ?t (cur_vars=[])
+        #    match: ?pe a ?ty
 
         match_conds = []; match_args = []; lmbda_params = [], ok = True
         for match in matches:
@@ -130,11 +151,14 @@ class GenPython:
                 match_r = match_clause[pos]; clause_r = clause[pos]
                 if match_r.is_concrete():
                     if clause_r.is_concrete():
+                        # ex 1: Person <> Canadian
                         if clause_r != match_r:  # compile-time check
                             ok = False
                             break
-                    else:  # add runtime check, if variable is incoming param
+                    else:  # add runtime check, if possible
                         if clause_r.name in in_params:
+                            # ex 2: t clause var is given as param
+                            # t is None or t == Iri(Canadian)
                             cmp1 = self.__builder.comp(self.__builder.ref(
                                 clause_r.name), 'is', self.__builder.cnst(None))
                             cmp2 = self.__builder.comp(self.__builder.ref(
@@ -143,29 +167,41 @@ class GenPython:
                 
                 else: # values will be passed as lambda parameters
                     if clause_r.is_concrete():
-                        lmbda_params.append('_') # don't care about value (will be same)
-                        match_args.append(self.__cnstr_call(clause_r))
+                        # not a variable so is not needed in ctu (ex 3)
+                        lmbda_params.append('_')
+                        match_args.append(self.__cnstr_call(clause_r)) # Iri(Person)
                     else:
-                        lmbda_params.append(clause_r.name) # (same as ctu_param)
-                        if clause_r.name in in_params:
+                        # ex 1-4 (p<>p, p<>pe, t<>ty)
+                        # always get value from match clause / find call; 
+                        # either we gave None, or it is the same as what we gave
+                        # (use our var's name, as it is same as ctu_param)
+                        lmbda_params.append(clause_r.name) # (ex: p, t)
+                        if clause_r.name in in_params: # ex 3
                             match_args.append(
                                 self.__builder.ref(clause_r.name))
-                        else:
+                        else: # ex 4
                             match_args.append(self.__builder.cnst(None))
 
             if ok:
+                # arguments to be passed to ctu
+                # ex 1, 2: p ; ex 3, 4: p, t
                 call_args = [self.__builder.ref(p) for p in ctu_params] + rest_args
                 ctu_call = self.__builder.fn_call(
                     self.__builder.ref(ctu_fn), call_args)
 
-                lmbda_params.append('state')
+                # lambda params: useful vars from matching clause
+                # ex 3: p, _; ex 4: p, t
+                lmbda_params.append('state') # (add rest param)
                 lmbda = self.__builder.lmbda(lmbda_params, ctu_call)
 
+                # match args: args we pass to match fn call
+                # ex 3: p (in param), Person (concrete); ex 4: None, None
                 match_args += [self.__builder.ref('model'),
                                self.__builder.ref('state'), lmbda]
                 match_call = self.__builder.to_stmt(self.__builder.fn_call(
                     self.__builder.ref(match.fn_name), match_args))
 
+                # if needed, wrap in runtime conditional
                 if len(match_conds) > 0:
                     match_call = self.__builder.ifc(
                         self.__builder.conj(match_conds), match_call)
@@ -173,9 +209,6 @@ class GenPython:
                 self.__builder.fn_body_stmt(
                     clause_fn, self.__builder.to_stmt(match_call))
 
-        ast.fix_missing_locations(clause_fn)
-
-        return clause_fn
 
     def __matching_rules(self, clause):
         if clause.p.type() == term_types.VAR:
